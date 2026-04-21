ea632133809d00767a61e0fc7b3727ca288f3495
Ajalon Phillips <phillipsajalon@gmail.com>
2026-04-21 14:02:07 -0500

    Implement snake movement and keyboard controls with automatic updates

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 
 public class SnakeGame {
@@ -12,19 +13,76 @@ public class SnakeGame {
         frame.setVisible(true);
     }
 
-    static class GamePanel extends JPanel {
+    static class GamePanel extends JPanel implements ActionListener, KeyListener {
         private final int WIDTH = 600;
         private final int HEIGHT = 600;
         private final int CELL_SIZE = 30;
         private final int GRID_WIDTH = WIDTH / CELL_SIZE;
         private final int GRID_HEIGHT = HEIGHT / CELL_SIZE;
         private java.util.List<Point> snake;
+        private javax.swing.Timer timer;
+        private static final int RIGHT = 0;
+        private static final int DOWN = 1;
+        private static final int LEFT = 2;
+        private static final int UP = 3;
+        private int direction = RIGHT;
 
         public GamePanel() {
             snake = new ArrayList<>();
             snake.add(new Point(8, 10));
             snake.add(new Point(9, 10));
             snake.add(new Point(10, 10));
+            timer = new javax.swing.Timer(150, this);
+            setFocusable(true);
+            addKeyListener(this);
+            timer.start();
+        }
+
+        private void move() {
+            Point head = snake.get(0);
+            int newX = head.x;
+            int newY = head.y;
+            if (direction == RIGHT) {
+                newX = (newX + 1) % GRID_WIDTH;
+            } else if (direction == LEFT) {
+                newX = (newX - 1 + GRID_WIDTH) % GRID_WIDTH;
+            } else if (direction == DOWN) {
+                newY = (newY + 1) % GRID_HEIGHT;
+            } else if (direction == UP) {
+                newY = (newY - 1 + GRID_HEIGHT) % GRID_HEIGHT;
+            }
+            snake.add(0, new Point(newX, newY));
+            snake.remove(snake.size() - 1);
+            repaint();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            move();
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_RIGHT && direction != LEFT) {
+                direction = RIGHT;
+            } else if (key == KeyEvent.VK_LEFT && direction != RIGHT) {
+                direction = LEFT;
+            } else if (key == KeyEvent.VK_DOWN && direction != UP) {
+                direction = DOWN;
+            } else if (key == KeyEvent.VK_UP && direction != DOWN) {
+                direction = UP;
+            }
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // Not used
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // Not used
         }
 
         @Override
